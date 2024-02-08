4a3ac31cbd051e52e744920dcf7de40d4eab13cc
Depe2788 <davide.devittorio01@universitadipavia.it>
2024-02-08 17:54:28 +0100

    Carica prova

--- Code/src/it/unipv/ingsw/UniBook/Controller/BookingController.java
+++ Code/src/it/unipv/ingsw/UniBook/Controller/BookingController.java
@@ -5,25 +5,27 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
-import it.unipv.ingsw.UniBook.Model.Prenotazione;
-import it.unipv.ingsw.UniBook.Model.Risorsa;
-import it.unipv.ingsw.UniBook.DB.RisorsaDAO;
+import it.unipv.ingsw.UniBook.Model.Booking;
+import it.unipv.ingsw.UniBook.Model.Resource;
+import it.unipv.ingsw.UniBook.DB.ResourceDAO;
 import it.unipv.ingsw.UniBook.View.BookingView;
-import it.unipv.ingsw.UniBook.DB.PrenotazioneDAO;
+import it.unipv.ingsw.UniBook.View.DeleteBookingView;
+import it.unipv.ingsw.UniBook.DB.BookingDAO;
 import it.unipv.ingsw.UniBook.Model.SingletonManager;
+import it.unipv.ingsw.UniBook.Exception.*;
+import it.unipv.ingsw.UniBook.View.DeleteBookingView;
 
 public class BookingController {
 
-	private Prenotazione model;
+	private Booking model;
 	private BookingView view;
-	private PrenotazioneDAO pDAO;
+	private DeleteBookingView dview;
 
 	public BookingController() {
 
 	}
 
-	public BookingController(Prenotazione model, BookingView view) {
-		this.pDAO = SingletonManager.getInstance().getPrenotazioneDAO();
+	public BookingController(Booking model, BookingView view) {
 		this.model = model;
 		this.view = view;
 		initComponents();
@@ -31,7 +33,7 @@ public class BookingController {
 
 	private void initComponents() {
 
-		ActionListener conferma = new ActionListener() {
+		ActionListener confirm = new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				manageAction();
@@ -40,33 +42,25 @@ public class BookingController {
 			private void manageAction() {
 
 				try {
-					model.setData(view.getData());
-					model.setOra(view.getOra());
-					model.setNome(view.getRisorsa());
-					model.setDurata(view.getDurata());
+					model.setDate(view.getData());
+					model.setTime(view.getOra());
+					model.setR(new Resource(0, view.getRisorsa(), null, null));
+					model.setDuration(view.getDurata());
 
-					if (model.isDurataValida()) {					
+					model.checkEmptyDate();
+					model.checkDuration();
 
-						boolean inserimentoRiuscito = pDAO.insertPrenotazione(model);
+					model.tryToBook();
 
-						if (inserimentoRiuscito) {
-							mostraPopup("Prenotazione effettuata con successo! " + "\n Data: " + model.getData()
-									+ "\n Dalle: " + model.getOra() + " alle: " + model.getFinePrenotazione()
-									+ "\n Risorsa: " + model.getNome());
-
-							view.dispose();
-						} else {
-							mostraPopup("Errore durante l'inserimento nel database.");
-						}
-
-					} else {
-						mostraPopup("La durata selezionata non è valida per l'orario di prenotazione."
-								+ "Ti ricordiamo che l'università chiude alle 19:00.");
-					}
-
-				} catch (NullPointerException e) {
-					mostraPopup("Seleziona una data");
-					e.notify();
+				} catch (DatabaseException e) {
+					e.mostraPopup();
+					System.out.println(e.toString());
+				} catch (EmptyFieldException e) {
+					e.mostraPopup();
+					System.out.println(e.toString());
+				} catch (DurationException e) {
+					e.mostraPopup();
+					System.out.println(e.toString());
 				}
 
 			}
@@ -74,35 +68,51 @@ public class BookingController {
 		};
 
 		// Aggiungo il listener al bottone
-		view.getConfermaButton().addActionListener(conferma);
+		view.getConfermaButton().addActionListener(confirm);
 
+		ActionListener remove = new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				manageAction();
+			}
+
+			private void manageAction() {
+				
+				dview = new DeleteBookingView();
+				dview.updateTable(model.getUserBookings(SingletonManager.getInstance().getLoggedUser()));
+				remove();
+				//dview.updateTable(model.getUserBookings(SingletonManager.getInstance().getLoggedUser()));
+			}
+
+		};
+
+		view.getRemoveButton().addActionListener(remove);
+		
 	}
+	
+	private void remove() {
+		
+		ActionListener deleteBooking=new ActionListener() {
+			
+			public void actionPerformed(ActionEvent e) {
+				manageAction();
+			}
 
-	public ArrayList<String> aggiornaJListRisorse() {
+			private void manageAction() {
+	            // Ottieni l'indice della riga selezionata dalla vista
+	            int selectedRow = dview.getSelectedRow();
+	            // Rimuovi la prenotazione dal modello	            
+	            model.removeBooking(model.getUserBookings(SingletonManager.getInstance().getLoggedUser()), selectedRow);
+	            
+	            
+	            // Aggiorna la vista
 
-		RisorsaDAO risorsaDAO = new RisorsaDAO();
-		ArrayList<String> risorseDisponibili = risorsaDAO.selectAll();
-
-		return risorseDisponibili;
-	}
-
-	public String[] sceltaOrario() {
-		// Creo un array di orari disponibili da 8 a 18
-		String[] orarioDisponibile = new String[11];
-		for (int i = 0; i < 11; i++) {
-			int ora = 8 + i;
-			orarioDisponibile[i] = String.format("%02d:00", ora); // Formatto l'ora come "HH:00"
-		}
-		return orarioDisponibile;
-	}
-
-	public Integer[] sceltaDurata() {
-		// Creo un array di durate disponibili da 1 a 4 ore
-		return new Integer[] { 1, 2, 3, 4 };
-	}
-
-	private void mostraPopup(String messaggio) {
-		JOptionPane.showMessageDialog(null, messaggio, "Sistema", JOptionPane.INFORMATION_MESSAGE);
+			}
+			
+		};
+		
+	dview.getDeleteButton().addActionListener(deleteBooking);
+		
 	}
 
 }
