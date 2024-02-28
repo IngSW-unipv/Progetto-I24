123ece2d81662042bccdf492e0fd6c88e76c23d5
Depe2788 <davide.devittorio01@universitadipavia.it>
2024-02-28 18:12:04 +0100

    Fix booking test

--- Code/src/it/unipv/ingsw/UniBook/Test/BookingTest.java
+++ Code/src/it/unipv/ingsw/UniBook/Test/BookingTest.java
@@ -11,6 +11,12 @@ import it.unipv.ingsw.UniBook.Model.Booking;
 import it.unipv.ingsw.UniBook.Model.User;
 import it.unipv.ingsw.UniBook.Model.Resource;
 
+import org.junit.runner.RunWith;
+import org.junit.runners.BlockJUnit4ClassRunner;
+
+
+@RunWith(BlockJUnit4ClassRunner.class)
+
 public class BookingTest {
 
 	private Booking b[];
@@ -18,23 +24,21 @@ public class BookingTest {
 	private Resource r2;
 	private Resource r3;
 	private User u1;
-	private User u2;
 
 	@Before
 	public void initTest() {
 		b = new Booking[3];
 		u1 = new User("S500816", "", "Studente", "", "", "", "");
-		u2 = new User("P501934", "", "Professore", "", "", "", "");
-		r1 = new Resource(8, "Postazione A24", "Postazione laboratorio A2 informatica", "INF", "P", 2, null);
-		r2 = new Resource(18, "Postazione C3", "Postazione laboratorio C elettronica", "ELE", "P", 4, null);
-		r3 = new Resource(5, "Postazione A21", "Postazione laboratorio A2 informatica", "INF", "P", 2, null);
+		r1 = new Resource(16, "Postazione C1", "Postazione laboratorio C elettronica", "ELE", "P", 5, null);
+		r2 = new Resource(6, "Postazione A22", "Postazione laboratorio A2 informatica", "INF", "P", 2, null);
+		r3 = new Resource(17, "Postazione C2", "Postazione laboratorio C elettronica", "ELE", "P", 5, null);
 
 	}
 
 	@Test
 	public void testBookingOk() {
 
-		b[0] = new Booking(r1, u1, "07/05/2021", "08:00:00", 1);
+		b[0] = new Booking(r1, u1, "06/05/2021", "08:00:00", 1);
 		b[1] = new Booking(r2, u1, "07/05/2021", "12:00:00", 2);
 		b[2] = new Booking(r3, u1, "07/05/2021", "15:00:00", 3);
 
@@ -42,28 +46,33 @@ public class BookingTest {
 			assertTrue(booking.tryToBook());
 		}
 		
+		clear();
+		
+		
+		
 	}
 
 	//Prenotazioni sovrapposte alle precedenti
 	@Test
 	public void testBookingNotOk1() {
 
-		b[0] = new Booking(r1, u1, "07/05/2021", "08:00:00", 1);
-		b[1] = new Booking(r2, u1, "07/05/2021", "11:00:00", 2);
-		b[2] = new Booking(r3, u1, "07/05/2021", "16:00:00", 2);
+		b[0] = new Booking(r1, u1, "06/05/2024", "09:00:00", 3);
+		b[1] = new Booking(r2, u1, "07/05/2024", "08:00:00", 1);
+		b[2] = new Booking(r3, u1, "06/05/2024", "09:00:00", 3);
 
 		for (Booking booking : b) {
 			assertFalse(booking.tryToBook());
 		}
-		clear();
+		
 	}
 		
 	
 	public void clear() {
 		
-		b[0] = new Booking(r1, u1, "2021-05-07", "08:00:00", 1);
+		b[0] = new Booking(r1, u1, "2021-05-06", "08:00:00", 1);
 		b[1] = new Booking(r2, u1, "2021-05-07", "12:00:00", 2);
 		b[2] = new Booking(r3, u1, "2021-05-07", "15:00:00", 3);
+	
 		
 		for (Booking booking : b) {
 			SingletonManager.getInstance().getBookingDAO().deleteSelectedBooking(booking);
